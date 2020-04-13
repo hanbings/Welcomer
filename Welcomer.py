4954f9cf5997be3577e786cfa19f1174f07097a8
hanbings <38599937+hanbings@users.noreply.github.com>
2020-04-13 10:21:41 +0800

    2020/4/13 将Title方法独立成了API
    将Title方法独立成了API

--- Welcomer.py
+++ Welcomer.py
@@ -1,46 +1,46 @@
 # -*- coding: utf-8 -*-
 
-
 # 插件被加载时的
 import requests
 
+from plugins.TitleAPI import seed_sub_title, ChatColor
 
+
+# 写入帮助
 def on_load(server, old_module):
-    server.add_help_message('§2[Welcomer]', ' §5联系作者 Hanbings 3219065882@qq.com')
+    server.logger.info('[Welcomer] 欢迎插件已启用')
+    server.add_help_message('§l[Welcomer]', ' §6用!!welcomer 查看插件信息')
+
 
 # 获取进入游戏消息
 def on_player_joined(server, player):
-    seed_color_sub_title(server, player, '欢迎', get_text(), 'white', 'yellow')
+    message = get_text()
+    seed_sub_title(server, player, '欢迎', message)
+    server.execute("tell" + " " + player + " " + message)
+
+
+# 接受指令
+def on_info(server, info):
+    tempInfo = str(info)
+    if '!!welcomer' in tempInfo:
+        if check_player_if_not_command(info):
+            server.say("§l[Welcomer] §6联系作者 Hanbings 3219065882@qq.com")
+            server.say("§l[Welcomer] §6所有语句均来自于一言开发者平台")
+            server.say("§l[Welcomer] §6一言开发者平台 https://developer.hitokoto.cn/")
 
 
 # 获取每日一句
 def get_text():
-    req = requests.get('https://v1.hitokoto.cn/?c=f&encode=text')
+    req = requests.get('https://v1.hitokoto.cn/?encode=text')
     return req.text
 
 
-# title轮子
-
-# 封装发送标题（在屏幕中间显示）
-def seed_title(server, playerName, message):
-    server.execute("title" + " " + playerName + " " + "title" + " " + "{\"text\":\"" + message + "\"}")
-
-
-# 封装带颜色的标题（在屏幕中间显示）
-def seed_color_title(server, playerName, message, color):
-    server.execute(
-        "title" + " " + playerName + " " + "title" + " " + "{\"text\":\"" + message + "\",\"color\":\"" + color + "\"}")
+# 判断指令是不是处于聊天语句最前方
+def check_player_if_not_command(info):
+    if str(info.content).index('!!welcomer') == 0:
+        return True
+    else:
+        return False
 
 
-# 封装带子标题的标题（在屏幕中间显示）
-def seed_sub_title(server, playerName, message, subMessage):
-    server.execute("title" + " " + playerName + " " + "subtitle" + " " + "{\"text\":\"" + subMessage + "\"}")
-    server.execute("title" + " " + playerName + " " + "title" + " " + "{\"text\":\"" + message + "\"}")
 
-
-# 封装带颜色和子标题的标题（在屏幕中间显示）
-def seed_color_sub_title(server, playerName, message, subMessage, color, subColor):
-    server.execute("title" + " " + playerName + " " + "subtitle" + " " + "{\"text\":\"" + subMessage + "\",\"color\":\""
-                   + subColor + "\"}")
-    server.execute("title" + " " + playerName + " " + "title" + " " + "{\"text\":\"" + message + "\",\"color\":\""
-                   + color + "\"}")
--- readme.md
+++ readme.md
@@ -8,6 +8,10 @@
 
 开发环境 MCDR 0.7.1
 
+以及一个前置插件 https://github.com/hanbings/TitleAPI
+
+使用方法相同
+
 ### 功能
 
 在玩家登录的时候发送一个标题到玩家屏幕
